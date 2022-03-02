8fd43f7ef515e26cacf55bd5e2e1d889cf519f51
gomez-git <78104957+gomez-git@users.noreply.github.com>
2022-03-02 21:00:33 +0300

    Update tests

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import os from 'os';
 import path from 'path';
 import nock from 'nock';
-import PageLoader from '../lib/page-loader.js';
+import pageLoader from '../lib/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,26 +12,35 @@ const readFile = (filename) => fs.promises.readFile(filename, 'utf-8');
 
 let tempdir;
 
-beforeEach(async () => {
+beforeAll(async () => {
   tempdir = await fs.promises.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
-  const data = await readFile(getFixturePath('expected.html'));
-  nock('https://ru.hexlet.io')
-    .get('/courses')
-    .reply(200, data);
 });
 
-afterEach(async () => {
+afterAll(async () => {
   await fs.promises.rm(tempdir, { recursive: true, force: true });
 });
 
 test('page loader', async () => {
+  const actual = await readFile(getFixturePath('actual.html'));
   const expected = await readFile(getFixturePath('expected.html'));
-  const pageLoader = new PageLoader('https://ru.hexlet.io/courses', tempdir);
-  await pageLoader
-    .getHtml()
-    .then(async (response) => {
-      await pageLoader.writeFile(response.data);
+  const png = await fs.promises.readFile(getFixturePath('nodejs.png'));
+  const pngpath = `${tempdir}/ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-nodejs.png`;
+
+  nock('https://ru.hexlet.io')
+    .get('/courses')
+    .reply(200, actual);
+  nock('https://ru.hexlet.io')
+    .get('/assets/professions/nodejs.png')
+    .reply(200, png);
+
+  await pageLoader('https://ru.hexlet.io/courses', tempdir)
+    .then(async (filepath) => {
+      await expect(readFile(filepath))
+        .resolves
+        .toEqual(expected);
     });
 
-  await expect(readFile(`${tempdir}/ru-hexlet-io-courses.html`)).resolves.toEqual(expected);
+  await expect(fs.promises.readFile(pngpath))
+    .resolves
+    .toEqual(png);
 });
